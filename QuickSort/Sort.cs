34653a4bf0a494e9a9a87889138346374531f5d3
Esarac <48232743+Esarac@users.noreply.github.com>
2020-09-19 18:26:47 -0500

    Model and Test without random :orange:

---
+++ QuickSort/Sort.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    public class Sort
+    {
+        //No Randomized
+        public void QuickSort(int[] a, int p, int r) {
+            if (p < r)
+            {
+                int q = Partition(a, p, r);
+                QuickSort(a, p, q - 1);
+                QuickSort(a, q + 1, r);
+            }
+        }
+
+        public int Partition(int[] a, int p, int r)
+        {
+            int x = a[r];
+            int i = p - 1;
+            for(int j = p; j <= (r - 1); j++)
+            {
+                if (a[j] <= x)
+                {
+                    i = i + 1;
+
+                    int aux = a[i];//Guarda
+                    a[i] = a[j];
+                    a[j] = aux;
+                }
+            }
+            a[i + 1] = a[r];
+
+            return i + 1;
+        }
+
+        //Randomized
+        public void RandomQuickSort(int[] a, int p, int r)
+        {
+            if(p < r)
+            {
+                int q = RandomPartition(a, p, r);
+                RandomQuickSort(a, p, q - 1);
+                RandomQuickSort(a, q + 1, r);
+            }
+        }
+
+        public int RandomPartition(int[] a, int p, int r)
+        {
+            //Random rnd = new Random();
+            //int i = rnd.Next(p, r);
+
+            //int aux = a[i];//Guarda
+            //a[i] = a[r];
+            //a[r] = aux;
+
+            return Partition(a, p, r);
+        }
+    }
+}
---
+++ QuickSortTest/SortTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuickSort;
+
+namespace QuickSortTest
+{
+    [TestClass]
+    public class SortTest
+    {
+        //Tested Class
+        private Sort sort;
+
+        //Scenes
+        [TestInitialize]
+        public void Initialize()
+        {
+            sort = new Sort();
+        }
+
+        //Tests
+        [TestMethod]
+        public void TestQuickSort()
+        {
+            //Small
+            int[] small = { 5, 2, 3, 1, 4};
+
+            sort.QuickSort(small, 0, small.Length - 1);
+            for(int i = 0; i < small.Length - 1; i++){
+                Assert.IsTrue(small[i] <= small[i + 1]);
+            }
+
+            //Big
+            int[] big = new int[1000];
+            for (int i = 0; i < big.Length; i++)
+            {
+                big[i] = 1000 - i;
+            }
+
+            sort.QuickSort(big, 0, big.Length - 1);
+            for (int i = 0; i < big.Length - 1; i++)
+            {
+                Assert.IsTrue(big[i] < big[i + 1]);
+            }
+
+            //Random
+            int[] random = new int[100];
+            for (int i = 0; i < random.Length; i++)
+            {
+                //random[i] = Random.Next();
+            }
+
+            sort.QuickSort(random, 0, random.Length - 1);
+            for (int i = 0; i < random.Length - 1; i++)
+            {
+                Assert.IsTrue(random[i] <= random[i + 1]);
+            }
+
+        }
+
+        [TestMethod]
+        public void TestPartition()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestRandomQuickSort()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestRandomPartition()
+        {
+
+        }
+
+    }
+}
