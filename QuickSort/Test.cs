1a538cf8621d813b3a8ac51cdc3159a5517c6a5c
VoodLyc <48293069+VoodLyc@users.noreply.github.com>
2020-09-22 00:09:07 -0500

    I add new classes
    I added two new classes (TestManager and Test) in order to do the tests (Not the automated tests).

---
+++ QuickSort/Test.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Test
+    {
+        //Constants
+        public const int RANDOM = 0;
+        public const int ASCENDING = 1;
+        public const int DESCENDING = 2;
+
+        //Attributes
+        private bool randomized;
+        private int order;
+        private int repetitions;
+        private int[] sizes;
+
+        //Properties
+        public bool Randomized
+        {
+            get { return randomized; }
+        }
+
+        public int Order
+        {
+            get { return order; }
+        }
+
+        public int Repetitions
+        {
+            get { return repetitions; }
+        }
+
+        public int[] Sizes
+        {
+            get { return sizes; }
+        }
+
+        //Constructor
+        public Test(bool randomized, int order, int repetitions, int[] sizes)
+        {
+            this.randomized = randomized;
+            this.order = order;
+            this.repetitions = repetitions;
+            this.sizes = sizes;
+        }
+    }
+}
---
+++ QuickSort/TestManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class TestManager
+    {
+        //Attributes
+        private List<Test> tests;
+        private List<string> rows;
+
+        //Constructor
+        public TestManager(List<Test> tests)
+        {
+            this.tests = tests;
+        }
+
+        //Methods
+        public void RunTests()
+        {
+            foreach(Test test in tests)
+            {
+                for(int i = 0; i < test.Repetitions; i++)
+                {
+
+                }
+            }
+        }
+
+
+        public void RunTest()
+        {
+
+        }
+
+        public List<Int32> CreateList(int size)
+        {
+            return null;
+        }
+    }
+}
